57f3581c56769fec577493ba20a89c3952d3506c
Fer Martínez <fr663385@gmail.com>
2024-02-29 14:30:57 -0600

    primer commit

---
+++ dist/hola.js
@@ -0,0 +1,5 @@
+// Fernando Martínez Rodríguez
+let mensaje = "Hola Mundo!!!";
+console.log(mensaje);
+//# sourceMappingURL=hola.js.map
+//# sourceMappingURL=hola.js.map
---
+++ src/hola.ts
@@ -0,0 +1,5 @@
+// Fernando Martínez Rodríguez
+let mensaje: string = "Hola Mundo!!!"
+console.log(mensaje);
+
+//# sourceMappingURL=hola.ts.map
